b83f522412af82eee54135202bf1fba5db5898dc
cary faline <caryfalineagudelo21@gmail.com>
2023-09-05 23:37:07 -0500

    se estructura la funcion para hacer pedidos

--- handler.js
+++ handler.js
@@ -1,50 +1,74 @@
 'use strict';
-const querystring = require("querystring");
+const querystring = require("querystring")
 const mysql=require('mysql');
 const connection=mysql.createConnection({
   host:'database-1.cz1ybvsh329r.us-east-1.rds.amazonaws.com',
   user:'admin',
-port:"3306",
-password:'12345678',
-database:'data base1',
+  port:'3306',
+  password:'12345678',
+  database:'proyectorestaurante',
 });
 
-module.exports.hacerpedido = async (event) => {
-  const pedido = querystring.parse(event["body"])
+module.exports.hacerPedido = async (event) => {
+  const pedidos = querystring.parse(event["body"])
+  await new Promise((resolve, reject) => {
+  const queryclient = "CALL insert_pedidos(?,?,?,?,?);";
+    connection.query(queryclient,[pedidos.producto_id,pedidos.cantidad_und,pedidos.valorUnidad,pedidos.valorTotal,pedidos.cliente_id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: "exitoso",
-        input: 'orden ${pedido.name}',
+        cliente_id: pedidos.cliente_id,
+        producto_id: pedidos.producto_id,
+        cantidad_und: pedidos.cantidad_und,
+        valorUnidad: pedidos.valorUnidad,
+        valorTotal: pedidos.valorTotal,  
       },
       null,
       2
     ),
   };
-
+  connection.end();
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
-module.exports.obtenerpedido = async (event) => {
+module.exports.obtenerPedido = async (event) => {
+  const pedido = querystring.parse(event["body"])
+  const queryclient = "SELECT * FROM proyectorestaurante.pedidos where id=?;";
+  const consulta = await new Promise((resolve, reject) => {
+     connection.query(queryclient,[pedido.id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+  connection.end();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'ejemplo de pedido',
-        input: event,
+        message: "exitoso",
+        pedido: consulta,
       },
       null,
       2
     ),
   };
+  
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-
-
-
-
-
 
 
 
